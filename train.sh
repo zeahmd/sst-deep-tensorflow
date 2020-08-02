a6345e1fb5aa7e188a86c8a87ac4b6db95c57367
Zeeshan Ahmad <zeeshanahmad10809@gmail.com>
2020-08-02 15:26:51 +0500

    updated train.sh

--- train.sh
+++ train.sh
@@ -1,1 +1,38 @@
 #!/bin/bash
+
+#environment variables
+MODELS=("rnn" "lstm" "bilstm" "gru" "conv1d")
+PATIENCE=3
+
+#variables
+patience=$PATIENCE
+
+#It will train total 20 models 4 for net.
+for model in "${MODELS[@]}"; do
+    set -x
+    #run all and fine
+        python run.py \
+        --name ${model} \
+        --patience ${patience} \
+        --save
+    #run root and fine
+        python run.py \
+        --name ${model} \
+        --root \
+        --patience ${patience} \
+        --save
+    #run all and binary
+        python run.py \
+        --name ${model} \
+        --binary \
+        --patience ${patience} \
+        --save
+    #run root and binary
+        python run.py \
+        --name ${model} \
+        --binary \
+        --root \
+        --patience ${patience} \
+        --save
+    set +x
+done
